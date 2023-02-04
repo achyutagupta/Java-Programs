2b4109ec4a73d2b13cebd393c6f161ec3a62bf3b
Achyuta <achyutagupta888rg@gmail.com>
2023-02-04 16:06:17 +0530

    Patterns

---
+++ Patterns/p11.java
@@ -0,0 +1,17 @@
+public class p11 {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=5-i+1;k<=5;k++){
+                if(k==6-i||k==5||i==5){
+                    System.out.print("* ");
+                }
+                else
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/p12.java
@@ -0,0 +1,30 @@
+public class p12 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=5-i+1;k<=5;k++){
+                if(k==6-i||k==5){
+                    System.out.print("* ");
+                }
+                else
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+        for(int i=4;i>=1;i--){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=5-i+1;k<=5;k++){
+                if(k==6-i||k==5){
+                    System.out.print("* ");
+                }
+                else
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
